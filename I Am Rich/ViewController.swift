df2b114a11ba30d2e6838d51ad5b59f5daee1c10
Wesley Sun <wsun@lcs.on.ca>
2020-03-24 22:09:08 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sun, Wesley on 2020-03-24.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
